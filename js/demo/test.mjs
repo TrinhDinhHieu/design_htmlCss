09f882962b1e616b83fc8a5f042fc970991f4933
Trinh Dinh Hieu <104412188+TrinhDinhHieu@users.noreply.github.com>
2023-05-05 14:18:02 +0700

    Delete js directory

--- js/demo/test.mjs
+++
@@ -1,3 +0,0 @@
-//trong 1 file chỉ export default 'duy nhất 1' đối tượng Fn duy nhất và khi import thì ko cần để trong { }, và có thể đặt tên khác
-
-// còn export thì có thể cho nhiều function // khi import cần pải đúng tên và để trong { }// nếu muốn đổi tên thì  dùng ' as ' 
